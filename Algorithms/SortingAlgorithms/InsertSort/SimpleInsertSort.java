7e5bdb09cefe9b16e4863e43602e46b6505888b5
ElbekM <61645317+ElbekM@users.noreply.github.com>
2020-03-13 02:42:26 +0700

    Update SimpleInsertSort.java

--- Algorithms/SortingAlgorithms/InsertSort/SimpleInsertSort.java
+++ Algorithms/SortingAlgorithms/InsertSort/SimpleInsertSort.java
@@ -1,7 +1,6 @@
 package Algorithms.SortingAlgorithms.InsertSort;
 
 import java.util.Arrays;
-
 import Algorithms.SortingAlgorithms.SortAlgorithm;
 
 /**
@@ -14,29 +13,28 @@ public class SimpleInsertSort extends SortAlgorithm {
 	@Override
 	public int[] sort(int[] array) {
 		int index, value;
-		
-        for (int element = 1; element < array.length; ++element) {
-        	value = array[element];
-        	index = element;
-        	while (index > 0 && array[index - 1] > value) {
-        		array[index] = array[index - 1];
-        		index--;
-        	}
-        	array[index] = value;
-        }
+		for (int element = 1; element < array.length; ++element) {
+			value = array[element];
+			index = element;
+			while (index > 0 && array[index - 1] > value) {
+				array[index] = array[index - 1];
+				index--;
+			}
+			array[index] = value;
+		}
 		return array;
 	}
 	
 	public int[] sortWithFor(int[] array) {
-		int ind;
-        for (int el = 0; el < array.length; ++el) {
-        	int tmp = array[el];
-        	for (ind = el - 1; ind >= 0 && tmp < array[ind]; ind--) {
-        		array[ind + 1] = array[ind];
-        	}
-        	array[ind + 1] = tmp;
-        }
-        return array;
+		int index;
+		for (int element = 0; element < array.length; ++element) {
+			int tmp = array[element];
+			for (index = element - 1; index >= 0 && tmp < array[index]; index--) {
+				array[index + 1] = array[index];
+			}
+			array[index + 1] = tmp;
+		}
+		return array;
 	}
 	
 	
