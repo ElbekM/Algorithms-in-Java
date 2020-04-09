575fbc7f42f73bf8de3dd796a9f4e9a005160ce3
ElbekM <elbek.987598@gmail.com>
2020-04-09 21:32:43 +0700

    Added Shell sort algorithm

--- Algorithms/SortingAlgorithms/ShellSort.java
+++ Algorithms/SortingAlgorithms/ShellSort.java
@@ -1,7 +1,17 @@
 package Algorithms.SortingAlgorithms;
 
+import java.util.Arrays;
+
 /**
- * Shell sort a
+ * Shell Sort
+ * 
+ * Modified insertion sort with gaps
+ * 
+ * Time Complexity: O(N**2) | O(N log**2N)
+ * 
+ * Space Complexity: O(1) Auxiliary
+ * 
+ * Stable: Yes
  * 
  * @author Elbek M
  */
@@ -10,16 +20,41 @@ public class ShellSort extends SortAlgorithm {
 
 	@Override
 	public int[] sort(int[] array) {
-		int h = 1;
-		while (h < array.length) {
-			h = (h + 1) * 3; 
+		int gap = 1;
+		while (gap < array.length / 3) {
+			gap = gap * 3 + 1;
 		}
-		
-		for (int i = 0; i < array.length; ++i) {
-			
+		while (gap > 0) {
+			insertSort(array, gap);
+			gap = (gap - 1) / 3;
 		}
-		
 		return array;
 	}
 
+	private int[] insertSort(int[] array, int gap) {
+		int index, value;
+		for (int i = gap; i < array.length; ++i) {
+			value = array[i];
+			index = i;
+			while (index > gap - 1 && array[index - gap] > value) {
+				array[index] = array[index - gap];
+				index -= gap;
+			}
+			array[index] = value;
+		}
+		return array;
+	}
+
+	
+	// Driver Programm
+	public static void main(String[] args) {
+
+		int[] array = { 5, 7, 2, 4, 5, 10 };
+		ShellSort shellSort = new ShellSort();
+		shellSort.sort(array);
+		System.out.println(Arrays.toString(array));
+
+		ShellSort randomArray = new ShellSort();
+		randomArray.displayTime(randomArray.generateRandomNumbers(100000, 100000));
+	}
 }
